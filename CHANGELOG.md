1fb427c610ae53508df52f698d111c99bb9a62ca
wtthornton <52006304+wtthornton@users.noreply.github.com>
2026-01-16 11:35:40 -0800

    Bump version to 3.5.19

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [3.5.19] - 2026-01-20
+
+### Changed
+- Version bump to 3.5.19
+
 ## [3.5.18] - 2026-01-16
 
 ### Added
--- tapps_agents/__init__.py
+++ tapps_agents/__init__.py
@@ -24,8 +24,8 @@ Example:
     ```
 """
 
-__version__: str = "3.5.18"
+__version__: str = "3.5.19"
 
 # Also expose as _version_ for compatibility with some import mechanisms
 # This helps with editable installs where __version__ might not be importable
-_version_: str = "3.5.18"
+_version_: str = "3.5.19"
