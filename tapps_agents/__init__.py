36ba44cd926b4b4eecc3e17c6954498440e813b3
wtthornton <52006304+wtthornton@users.noreply.github.com>
2026-01-23 14:20:06 -0800

    Bump version to 3.5.29

--- tapps_agents/__init__.py
+++ tapps_agents/__init__.py
@@ -24,8 +24,8 @@ Example:
     ```
 """
 
-__version__: str = "3.5.28"
+__version__: str = "3.5.29"
 
 # Also expose as _version_ for compatibility with some import mechanisms
 # This helps with editable installs where __version__ might not be importable
-_version_: str = "3.5.28"
+_version_: str = "3.5.29"
